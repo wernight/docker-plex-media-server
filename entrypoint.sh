83a47da59f0aabe78c6997cec1188398549e7b58
Werner Beroux <werner@beroux.com>
2016-03-09 10:32:03 +0100

    Improves initial setup.
      - Renames and unify environment variables (fallback supported):
          PLEX_EXTERNALPORT -> PLEX_EXTERNAL_PORT
          PLEXPASS_LOGIN -> PLEX_LOGIN
          PLEX_USERNAME -> PLEX_LOGIN
          PLEXPASS_PASSWORD -> PLEX_PASSWORD
      - Supports X_PLEX_TOKEN during initial setup if provided (previously required
        PLEX_USERNAME and PLEX_PASSWORD).
      - Removes PLEX_LOGIN, PLEX_PASSWORD... from environment before
        running Plex.
      - Uses default Preferences.xml even if no username/password/token is
        provided so that for example PLEX_EXTERNAL_PORT can be used.
      - Document PLEX_EXTERNAL_PORT.
    
    This should improve for issues #9 and issue #23.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,41 +1,76 @@
 #!/bin/sh -e
 
+# Legacy environment variables support.
+if [ -n "$PLEX_USERNAME" ]; then
+    echo "WARNING: 'PLEX_USERNAME' has been deprecated and is now called 'PLEX_LOGIN'."
+    PLEX_LOGIN="$PLEX_USERNAME"
+    unset PLEX_USERNAME
+fi
+
+if [ -n "$PLEXPASS_LOGIN" ]; then
+    echo "WARNING: 'PLEXPASS_LOGIN' has been deprecated and is now called 'PLEX_LOGIN'."
+    PLEX_LOGIN="$PLEXPASS_LOGIN"
+    unset PLEXPASS_LOGIN
+fi
+
+if [ -n "$PLEXPASS_PASSWORD" ]; then
+    echo "WARNING: 'PLEXPASS_PASSWORD' has been deprecated and is now called 'PLEX_PASSWORD'."
+    PLEX_PASSWORD="$PLEXPASS_PASSWORD"
+    unset PLEXPASS_PASSWORD
+fi
+
+if [ -n "$PLEX_EXTERNALPORT" ]; then
+    echo "WARNING: 'PLEXEXTERNALPORT' has been deprecated and is now called 'PLEX_EXTERNAL_PORT'."
+    PLEX_EXTERNAL_PORT="$PLEXPASS_EXTERNALPORT"
+    unset PLEXPASS_EXTERNALPORT
+fi
+
 # Delete PID file (we don't use that)
-if [ -f /config/Plex\ Media\ Server/plexmediaserver.pid ]
-then
+if [ -f /config/Plex\ Media\ Server/plexmediaserver.pid ]; then
     rm -f /config/Plex\ Media\ Server/plexmediaserver.pid
 fi
 
-# Get plex token if PLEX_USERNAME and PLEX_PASSWORD are define
-[ "${PLEX_USERNAME}" ] && [ "${PLEX_PASSWORD}" ] && {
+# Get plex token if Plex username and password are defined.
+if [ -n "$PLEX_LOGIN" ] && [ -n "$PLEX_PASSWORD" ]; then
+    # Retrieve a token from Plex.tv
+    X_PLEX_TOKEN=$(curl -u "${PLEX_LOGIN}":"${PLEX_PASSWORD}" 'https://plex.tv/users/sign_in.xml' \
+        -X POST -H 'X-Plex-Device-Name: PlexMediaServer' \
+        -H 'X-Plex-Provides: server' \
+        -H 'X-Plex-Version: 0.9' \
+        -H 'X-Plex-Platform-Version: 0.9' \
+        -H 'X-Plex-Platform: xcid' \
+        -H 'X-Plex-Product: Plex Media Server'\
+        -H 'X-Plex-Device: Linux'\
+        -H 'X-Plex-Client-Identifier: XXXX' --compressed | sed -n 's/.*<authentication-token>\(.*\)<\/authentication-token>.*/\1/p')
+fi
 
-  # Ask Plex.tv a token key
-  TOKEN=$(curl -u "${PLEX_USERNAME}":"${PLEX_PASSWORD}" 'https://plex.tv/users/sign_in.xml' \
-    -X POST -H 'X-Plex-Device-Name: PlexMediaServer' \
-    -H 'X-Plex-Provides: server' \
-    -H 'X-Plex-Version: 0.9' \
-    -H 'X-Plex-Platform-Version: 0.9' \
-    -H 'X-Plex-Platform: xcid' \
-    -H 'X-Plex-Product: Plex Media Server'\
-    -H 'X-Plex-Device: Linux'\
-    -H 'X-Plex-Client-Identifier: XXXX' --compressed | sed -n 's/.*<authentication-token>\(.*\)<\/authentication-token>.*/\1/p')
-
-  if [ ! -f /config/Plex\ Media\ Server/Preferences.xml ]; then
+# Default Preferences.
+if [ ! -f /config/Plex\ Media\ Server/Preferences.xml ]; then
     mkdir -p /config/Plex\ Media\ Server
     cp /Preferences.xml /config/Plex\ Media\ Server/Preferences.xml
-  fi
+fi
 
-  if [ ! $(xmlstarlet sel -T -t -m "/Preferences" -v "@PlexOnlineToken" -n /config/Plex\ Media\ Server/Preferences.xml) ]; then
-    xmlstarlet ed --inplace --insert "Preferences" --type attr -n PlexOnlineToken -v ${TOKEN} /config/Plex\ Media\ Server/Preferences.xml
-  fi
+# Sets PlexOnlineToken in Preferences.xml if provided.
+if [ -n "$X_PLEX_TOKEN" ]; then
+    #if [ ! $(xmlstarlet sel -T -t -m "/Preferences" -v "@PlexOnlineToken" -n /config/Plex\ Media\ Server/Preferences.xml) ]; then
+        xmlstarlet ed --inplace --insert "Preferences" --type attr -n PlexOnlineToken -v ${X_PLEX_TOKEN} /config/Plex\ Media\ Server/Preferences.xml
+    #fi
+fi
 
-  if [ "${PLEX_EXTERNALPORT}" ]; then
-    xmlstarlet ed --inplace --insert "Preferences" --type attr -n ManualPortMappingPort -v ${PLEX_EXTERNALPORT} /config/Plex\ Media\ Server/Preferences.xml
-  fi
+# Sets ManualPortMappingPort in Preferences.xml if provided.
+if [ -n "$PLEX_EXTERNAL_PORT" ]; then
+    xmlstarlet ed --inplace --insert "Preferences" --type attr -n ManualPortMappingMode -v 1 /config/Plex\ Media\ Server/Preferences.xml
+    xmlstarlet ed --inplace --insert "Preferences" --type attr -n ManualPortMappingPort -v ${PLEX_EXTERNAL_PORT} /config/Plex\ Media\ Server/Preferences.xml
+fi
 
-}
+# Unset any environment variable we used (just for safety as we don't need them anymore).
+unset PLEX_LOGIN
+unset PLEX_PASSWORD
+unset PLEX_EXTERNAL_PORT
+unset X_PLEX_TOKEN
 
 # Set the stack size
 ulimit -s $PLEX_MAX_STACK_SIZE
 
+echo "Starting Plex..."
 exec "$@"
